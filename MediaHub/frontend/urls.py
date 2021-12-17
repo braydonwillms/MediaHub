2dfa064e9e2049e87fd405b53d0ec3a01577df21
raymond-vong <60626161+raymond-vong@users.noreply.github.com>
2021-12-16 20:59:30 -0700

    fixed action="." for submission buttons

--- MediaHub/frontend/urls.py
+++ MediaHub/frontend/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
 	path('', views.index, name='index'),
-	path('index', views.index, name='index'),
-	path('dashboard', views.dashboard, name='dashboard'),
-	path('login', views.login, name='login'),
-	path('logout', views.logout, name='logout'),
-	path('register', views.register, name='register'),
+	path('index/', views.index, name='index'),
+	path('dashboard/', views.dashboard, name='dashboard'),
+	path('login/', views.login, name='login'),
+	path('logout/', views.logout, name='logout'),
+	path('register/', views.register, name='register'),
 ]
